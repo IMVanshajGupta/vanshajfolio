4aea696e53a35f67e52349167a90141bf14fcb8a
IMVanshajGupta <128994223+IMVanshajGupta@users.noreply.github.com>
2023-04-09 20:42:34 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,11 +1,67 @@
-const downloadBtn = document.getElementById('download-btn');
-const image = document.querySelector('img');
+body {
+    margin: 0;
+    padding: 0;
+}
 
-downloadBtn.addEventListener('click', () => {
-	const link = document.createElement('a');
-	link.href = image.src;
-	link.download = 'images/background.png';
-	document.body.appendChild(link);
-	link.click();
-	document.body.removeChild(link);
-});
+.pdf-container {
+    position: relative;
+    padding-bottom: 60%;
+    /* Set the aspect ratio for the PDF viewer */
+    height: 0;
+    margin: 20px;
+}
+
+.pdf-viewer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border: none;
+}
+
+.pdf-controls {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    text-align: center;
+    margin: 20px;
+}
+
+.pdf-download {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: #3498db;
+    color: #fff;
+    text-decoration: none;
+    border-radius: 5px;
+    font-size: 16px;
+    font-weight: bold;
+}
+
+.pdf-download:hover {
+    background-color: #2980b9;
+}
+
+@media only screen and (max-width: 768px) {
+    .pdf-container {
+        margin: 10px;
+    }
+}
+
+@media only screen and (max-width: 480px) {
+    .pdf-container {
+        padding-bottom: 80%;
+        margin: 5px;
+    }
+
+    .pdf-controls {
+        margin: 10px;
+    }
+
+    .pdf-download {
+        font-size: 14px;
+        padding: 8px 16px;
+    }
+}
